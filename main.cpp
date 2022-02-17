10f37f7aed8a569dcb611c9cbd3d684a26086f4d
bernardo <bernardomny@hotmail.com>
2022-02-17 02:30:46 -0300

    adicionando menu

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+void callCompression() {
+
+
+
+}
+
+void callDecompression() {
+
+
+
+}
+
+void callMetrics() {
+
+
+
+}
+
+void callMenu() {
+
+    int n;
+
+    cout << "qual operac,a~o voce^ deseja executar: \n[1]Comprimir reviews \n[2]Descomprimir arquivo bina'rio \n[3]Executar uma seque^ncia de compresso~es e computar me'tricas" << endl;
+
+    cin >> n;
+
+    switch(n) {
+
+        case 1:
+            callCompression();
+            break;
+        case 2:
+            callDecompression();
+            break;
+        case 3:
+            callMetrics();
+            break;
+        default:
+            cout << "Operac,a~o inva'lida. Tente novamente." << endl;
+            callMenu();
+            break;
+    }
+
+}
+
+int main()
+{
+
+    callMenu();
+
+    return 0;
+}
